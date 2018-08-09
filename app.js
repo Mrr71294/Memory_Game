eb76be9031caffb3643a3b5af076dec349b03897
Michael Rendon <mrr71294@gmail.com>
2018-08-09 14:01:58 -0400

    Finished Card flip and pairing logic

--- app.js
+++ app.js
@@ -1,57 +1,62 @@
+var score = 0;
+var pairsCorrect = 0;
+var selectedCards = [];
+var faceUpCards = [];
+var canSelectCard = true;
 var cards = [
-    { name:"c++",        img:"cpp-logo.png" },
-    { name:"c++",        img:"cpp-logo.png" },
-    { name:"css",        img:"css-logo.png" },
-    { name:"css",        img:"css-logo.png" },
-    { name:"html5",      img:"html5-logo.png" },
-    { name:"html5",      img:"html5-logo.png" },
-    { name:"java",       img:"java-logo.png" },
-    { name:"java",       img:"java-logo.png" },
-    { name:"javascript", img:"javascript-logo.jpg" },
-    { name:"javascript", img:"javascript-logo.jpg" },
-    { name:"mongodb",    img:"mongodb-logo.jpg" },
-    { name:"mongodb",    img:"mongodb-logo.jpg" },
-    { name:"node",       img:"node-logo.png" },
-    { name:"node",       img:"node-logo.png" },
-    { name:"php",        img:"php-logo.png" },
-    { name:"php",        img:"php-logo.png" },
-    { name:"python",     img:"python-logo.jpg" },
-    { name:"python",     img:"python-logo.jpg" },
-    { name:"ruby",      img:"ruby-logo.jpg" },
-    { name:"ruby",      img:"ruby-logo.jpg" },
-    { name:"ruby",      img:"ruby-logo.jpg" },
-    { name:"ruby",      img:"ruby-logo.jpg" },
-    { name:"ruby",      img:"ruby-logo.jpg" },
-    { name:"ruby",      img:"ruby-logo.jpg" },
+    { name:"cpp",        img:"cpp-logo.png",        instance:"1" },
+    { name:"cpp",        img:"cpp-logo.png",        instance:"2" },
+    { name:"css",        img:"css-logo.png",        instance:"1" },
+    { name:"css",        img:"css-logo.png",        instance:"2" },
+    { name:"html5",      img:"html5-logo.svg",      instance:"1" },
+    { name:"html5",      img:"html5-logo.svg",      instance:"2" },
+    { name:"java",       img:"java-logo.png",       instance:"1" },
+    { name:"java",       img:"java-logo.png",       instance:"2" },
+    { name:"javascript", img:"javascript-logo.jpg", instance:"1" },
+    { name:"javascript", img:"javascript-logo.jpg", instance:"2" },
+    { name:"mongodb",    img:"mongodb-logo.jpg",    instance:"1" },
+    { name:"mongodb",    img:"mongodb-logo.jpg",    instance:"2" },
+    { name:"node",       img:"node-logo.png",       instance:"1" },
+    { name:"node",       img:"node-logo.png",       instance:"2" },
+    { name:"php",        img:"php-logo.png",        instance:"1" },
+    { name:"php",        img:"php-logo.png",        instance:"2" },
+    { name:"python",     img:"python-logo.jpg",     instance:"1" },
+    { name:"python",     img:"python-logo.jpg",     instance:"2" },
+    { name:"ruby",       img:"ruby-logo.png",       instance:"1" },
+    { name:"ruby",       img:"ruby-logo.png",       instance:"2" },
+    { name:"angular2",   img:"angular2-logo.png",   instance:"1" },
+    { name:"angular2",   img:"angular2-logo.png",   instance:"2" },
+    { name:"react",      img:"react-logo.png",      instance:"1" },
+    { name:"react",      img:"react-logo.png",      instance:"2" },
   ];
 
-  function shuffle(array) {
-    var i = 0 , j = 0 , temp = null;
+//A function for to shuffle an array////////////////////////////////////////////
+function shuffle(array) {
+  var i = 0 , j = 0 , temp = null;
 
-    for (i = array.length - 1; i > 0; i -= 1) {
-      j = Math.floor(Math.random() * (i + 1));
-      temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
-
+}
+//shuffles the array of cards///////////////////////////////////////////////////
 function shuffleCards(cardsArray){
   shuffle(cards);
-
 }
 
-
+//Function that takes in an array of cards and turns it into html to be injected/
 function displayCards(cardsArray){
   let html = '';
   cardsArray.forEach(function(card){
-    html += '<div class="flip-container">';
+    html += '<div class="flip-container ' + card.name + '" id="' + card.name + card.instance + '">';
     html += '<div class="flipper">';
     html += '<div class="front">';
     html += '<img src="images/' + card.img + '" alt="' + card.name + '">';
     html += '</div>';
     html += '<div class="back">';
-    html += 'The cat is very small and thin.';
+    html += '<img src="images/card-back.png" alt="Back of card">';
     html += '</div>';
     html += '</div>';
     html += '</div>';
@@ -59,15 +64,59 @@ function displayCards(cardsArray){
   $("#cards-container").html(html);
 }
 
-displayCards(cards);
+//Event Listner to turn cards when clicked.//
+function listenForCardFlip(){
+  $(".flip-container").click(function(){
+    if (canSelectCard === false) {
+      return;
+    }
 
-
-
-
-
-
-
-
-$(".flip-container").click(function(){
     $(this).toggleClass("flip");
+    if (faceUpCards[0] === $(this).attr('id')) {
+        selectedCards = [];
+        faceUpCards =[];
+        return;
+    }
+    selectedCards.push($(this).attr('class').split(' ')[1]);
+    faceUpCards.push($(this).attr('id'));
+    checkSelectedCards();
+  });
+}
+
+//Function that filps the cards in the cardsUp array back down after a time out.//
+function flipCardsBackDown(){
+  setTimeout(function(){
+    $("#" + faceUpCards[0] + "").toggleClass("flip");
+    $("#" + faceUpCards[1] + "").toggleClass("flip");
+    faceUpCards = [];
+    selectedCards = [];
+    canSelectCard = true;
+  }, 750);
+}
+
+//Function that checks if both selected cards are correct or not, calls flipCardsBackDown when done.//
+function checkSelectedCards(){
+  if (selectedCards.length === 2) {
+    canSelectCard = false;
+    if (selectedCards[0] === selectedCards[1]) {
+      pairsCorrect += 1;
+      console.log("Pairs Correct:" + pairsCorrect);
+      flipCardsBackDown();
+    }
+    else {
+      flipCardsBackDown();
+    }
+  }
+}
+
+//Event Listner to turn ALL cards when clicked.//
+$(".header").click(function(){
+    console.log("hello!!!!");
+    $(".flip-container").toggleClass("flip");
 });
+
+
+
+// shuffleCards(cards);
+displayCards(cards);
+listenForCardFlip();
